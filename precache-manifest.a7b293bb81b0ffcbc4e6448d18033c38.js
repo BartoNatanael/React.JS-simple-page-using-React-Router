81a95bd08772d0651ea16cd20ed4bd456ba06dbd
BartoNatanael <bartlomiej.krzysztof.nowak@gmail.com>
2020-04-07 10:34:20 +0200

    Updates

---
+++ precache-manifest.a7b293bb81b0ffcbc4e6448d18033c38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c187d1446ed04e3d273536e948b16a",
+    "url": "/Simple-page-using-React-Router/index.html"
+  },
+  {
+    "revision": "da0d3b90283d8425288e",
+    "url": "/Simple-page-using-React-Router/static/css/main.6902e846.chunk.css"
+  },
+  {
+    "revision": "80aac08741a24f8f936f",
+    "url": "/Simple-page-using-React-Router/static/js/2.f3be31ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simple-page-using-React-Router/static/js/2.f3be31ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0d3b90283d8425288e",
+    "url": "/Simple-page-using-React-Router/static/js/main.7c07f52c.chunk.js"
+  },
+  {
+    "revision": "88f80f6eb4a4f288fd77",
+    "url": "/Simple-page-using-React-Router/static/js/runtime-main.dd7eacf4.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/Simple-page-using-React-Router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/Simple-page-using-React-Router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/Simple-page-using-React-Router/static/media/header3.d9d36b65.jpg"
+  }
+]);
